f20d7a99008d0c6ae04778e76a25879c6b68d51c
tosine6 <you@example.com>
2020-01-08 15:48:18 +0100

    changed requirements.txt

--- gt_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!c:\users\user\desktop\gtclone\goodtime-django-\gt_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
