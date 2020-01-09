8ccc1e4b7a103c2e8486047a8888a0862932a370
tosine6 <you@example.com>
2020-01-09 17:16:58 +0100

    made changes to templates

---
+++ gt_env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\user\desktop\gtclone\goodtime-django-\gt_env\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
