5837e413ef5dd4b7751a2a4d2cb4992c6690eb30
tosine6 <adetosine6@gmail.com>
2020-01-17 15:19:20 +0100

    second commit

--- goodtime/settings.py
+++ goodtime/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-# import django_heroku
+import django_heroku
 
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
@@ -119,7 +119,7 @@ USE_L10N = True
 
 USE_TZ = True
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
